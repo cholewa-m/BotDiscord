0b6abfb82b92066f16e7dd5f92d29adc40097d25
Mateusz Cholewa <m.cholewa@vp.pl>
2023-04-18 22:08:44 +0200

    bot can send messages

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,17 +1,13 @@
 package com.example
 
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import com.example.plugins.*
-import com.jessecorbett.diskord.bot.*
 import com.jessecorbett.diskord.util.*
 import com.jessecorbett.diskord.api.channel.ChannelClient
 import com.jessecorbett.diskord.internal.client.RestClient
 import kotlinx.coroutines.coroutineScope
 
-private const val BOT_TOKEN = "MTA5NzkxMTU4MzYzNzc4MjY2MA.GDfIHZ.kO8ZpeTW4aU9tjwUAwLED0FZ_TDFzOsSG_hRfk"  //MTA5NzkxMTU4MzYzNzc4MjY2MA.GDfIHZ.kO8ZpeTW4aU9tjwUAwLED0FZ_TDFzOsSG_hRfk
-private const val CHANNEL_ID = "1097901314920431660"    //1097901314920431660
+
+private const val BOT_TOKEN = "your-bot-token"
+private const val CHANNEL_ID = "your-channel-id"
 
 
 suspend fun main(): Unit = coroutineScope {
@@ -19,7 +15,7 @@ suspend fun main(): Unit = coroutineScope {
     val client = RestClient.default(BOT_TOKEN)
     val channel = ChannelClient(CHANNEL_ID, client)
 
-    println("You can now send messages to your channel! ")
+    println("You can now send messages to your channel using your bot! ")
 
     while(true) {
         print("Enter your message: ")
